d22e6153c279b6638beac089daa0b6f86cf36151
Mitko Arsov <58328017+mitkoarsov@users.noreply.github.com>
2021-06-30 15:36:34 +0200

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,7 @@ import { PureComponent } from "react";
 export interface GistProps {
   id: string;
   file?: string;
+  fitle?: string;
 }
 
 declare class Gist extends PureComponent<GistProps, any> {}
